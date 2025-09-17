7f467b8669b87fbba681f3e70cb635aa4de40423
Desmond Ubi <ubidesmond31@gmail.com>
2025-09-17 14:08:53 +0100

    added PUT to my cors configuration

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 // Add CORS middleware
 app.use(cors({
     origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://localhost:5177', 'http://localhost:5178'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 
