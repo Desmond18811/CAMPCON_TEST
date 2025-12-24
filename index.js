5606fcd3b6ce155b95687a10e396baf7280da92e
Desmond Ubi <ubidesmond31@gmail.com>
2025-12-24 21:32:43 +0100

    feat: Add `https://camcon.vercel.app` to CORS allowed origins.

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 3000;
 
 // Add CORS middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://localhost:5177', 'http://localhost:5178', 'https://campcon-test.onrender.com'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://localhost:5177', 'http://localhost:5178', 'https://campcon-test.onrender.com', 'https://camcon.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
